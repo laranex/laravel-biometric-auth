2d1d011b4dbd9a26ee84e85f48804739cf10d5c3
Nay Thu Khant <naythukhant644@gmail.com>
2023-12-13 10:41:49 +0630

    modification for RSA based complted

--- src/LaravelBiometricAuth.php
+++ src/LaravelBiometricAuth.php
@@ -34,7 +34,7 @@ class LaravelBiometricAuth
 
         throw_if(! $biometric->challenge, new BiometricChallengeNotFoundException());
 
-        return openssl_verify($biometric->challenge, base64_decode($signature), "-----BEGIN PUBLIC KEY-----\n" . $biometric->public_key . "\n-----END PUBLIC KEY-----", config('biometric-auth.signature_algorithm'));
+        return openssl_verify($biometric->challenge, base64_decode($signature), "-----BEGIN RSA PUBLIC KEY-----\n".$biometric->public_key."\n-----END RSA PUBLIC KEY-----", config('biometric-auth.signature_algorithm'));
     }
 
     /**
