2913f7c9a692ae02f89f4bd44c0b0d7c4fa89879
Nay Nyein Chan <naychan700@gmail.com>
2024-02-02 11:03:16 +0630

    change to laravel package provider from spatie package

--- src/LaravelBiometricAuthServiceProvider.php
+++ src/LaravelBiometricAuthServiceProvider.php
@@ -2,22 +2,65 @@
 
 namespace Laranex\LaravelBiometricAuth;
 
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+//use Spatie\LaravelPackageTools\Package;
+//use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
-class LaravelBiometricAuthServiceProvider extends PackageServiceProvider
+
+class LaravelBiometricAuthServiceProvider extends ServiceProvider
 {
-    public function configurePackage(Package $package): void
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('laravel-biometric-auth')
-            ->hasConfigFile()
-            ->hasMigration('create_biometrics_table')
-            ->runsMigrations();
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/biometric-auth.php', 'biometric-auth'
+        );
     }
+
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPublishing();
+
+        $this->registerMigrations();
+    }
+
+    /**
+     * Register the package's migrations.
+     *
+     * @return void
+     */
+    private function registerMigrations()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        }
+    }
+
+    /**
+     * Register the package's publishable resources.
+     *
+     * @return void
+     */
+    private function registerPublishing()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../database/migrations' => database_path('migrations'),
+            ], 'biometric-auth-migrations');
+
+            $this->publishes([
+                __DIR__ . '/../config/biometric-auth.php' => config_path('biometric-auth.php'),
+            ], 'biometric-auth-config');
+        }
+    }
+
 }
